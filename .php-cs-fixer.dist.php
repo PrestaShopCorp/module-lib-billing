74f0a6c0b24f8c7765ce57c4c3cbdff6e88891de
Jean-Marie COLLIN <jCollin@PRESTASHOP.local>
2022-02-14 21:38:54 +0100

    refactor: remove Config to avoir error with old PHP version

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,8 @@ $config = new PrestaShop\CodingStandards\CsFixer\Config();
 
 /** @var \Symfony\Component\Finder\Finder $finder */
 $finder = $config->setUsingCache(true)->getFinder();
-$finder->in(__DIR__)->exclude('vendor');
+$finder->in(__DIR__)
+  ->exclude('vendor')
+  ->notPath('src/Config/Config.php');
 
 return $config;
